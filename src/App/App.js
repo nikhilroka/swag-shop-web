11c22cdaf56f45df206bbdafa03f15ad6db3d514
Nikhil Rokka <nikhil.rokka@gmail.com>
2020-08-17 17:51:56 +0545

    My First Commit

---
+++ src/App/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Product from '../product/product';
+import HttpService from '../services/http-service';
+
+const http = new HttpService();
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {products:[]};
+        
+        //Bind functions
+        this.loadData = this.loadData.bind(this);
+        this.productList = this.productList.bind(this);
+        
+        this.loadData();
+    }
+    
+    loadData = () => {
+        var self = this;
+        http.getProducts().then(data => {
+            self.setState({products: data})
+        }, err => {
+            
+        });
+    }
+    
+    productList = () => {
+        const list = this.state.products.map((product) => 
+             <div className="col-sm-4" key={product._id}>
+                <Product title={product.title} price={product.price} imgUrl={product.imgUrl}/>
+            </div>
+        );
+    
+    return (list);  
+        
+    }
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <div className="container App-main">
+            <div className="row">
+            {this.productList()}
+            </div>
+          </div>
+         
+          
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
